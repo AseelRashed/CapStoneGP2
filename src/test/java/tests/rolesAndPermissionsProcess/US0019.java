b48a274f61cc2ba2b7baffdfaac5c6443a5e7236
D-almasaud <you@example.com>
2024-05-14 12:16:46 +0300

    Final

--- src/test/java/tests/rolesAndPermissionsProcess/US0019.java
+++ src/test/java/tests/rolesAndPermissionsProcess/US0019.java
@@ -4,15 +4,19 @@ package tests.rolesAndPermissionsProcess;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.RolesAndPermissionsPage;
+import utilities.Driver;
 
 public class US0019  {
-    HomePage homePage = new HomePage();
-    RolesAndPermissionsPage rolesAndPermissionsPage = new RolesAndPermissionsPage();
+    HomePage homePage ;
+    RolesAndPermissionsPage rolesAndPermissionsPage ;
 
     @Test
     public void TC02() {
+        homePage = new HomePage();
+        rolesAndPermissionsPage = new RolesAndPermissionsPage();
         homePage.roleClick();
         rolesAndPermissionsPage.verifyAllElementsEnabled();
+        Driver.tearDown();
 
     }
 }
